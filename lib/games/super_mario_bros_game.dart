50fb19cac6f242da22615f1d49b6716dc50abc68
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-24 13:27:49 -0300

    Autocommit a cada 15 minutos

--- lib/games/super_mario_bros_game.dart
+++ lib/games/super_mario_bros_game.dart
@@ -4,7 +4,6 @@ import 'package:flame_tiled/flame_tiled.dart';
 import 'package:recre_mario_bros_flutter/constants/globals.dart';
 import 'package:flame/components.dart';
 
-
 class SuperMarioBrosGame extends FlameGame {
   // RF Criando as variaveis todos os componetes destas variaveis abaixo estão no (package:flame/camera.dart)
   late CameraComponent cameraComponent_RF;
@@ -19,17 +18,18 @@ class SuperMarioBrosGame extends FlameGame {
     );
     world_RF.add(map_RF);
     // cameraComponent_RF = CameraComponent(world: world_RF)
-    // // RF Criar componente de cameras
-    // /* visor controla qual parte do mundo esta sendo vista através da janela de visualização 
-    // a janela de visualização é essencialmente uma janela a partir da qual o jogo é observado
-    // */
-    //   ..viewport.size = Vector2(100, 50);
-      // ..viewport.position = Vector2(500, 0)
-      // ..viewfinder.visibleGameSize = Vector2(450, 50)
-      // ..viewfinder.position = Vector2(0, 0)
-      // ..viewfinder.anchor = Anchor.topLeft;
+    //   // RF Criar componente de cameras
+    //   /* visor controla qual parte do mundo esta sendo vista através da janela de visualização
+    //   a janela de visualização é essencialmente uma janela a partir da qual o jogo é observado
+    //   */
+    //   ..viewfinder.visibleGameSize = Vector2(50, 50)
+    //   ..viewfinder.position = Vector2(0, 0)
+    //   ..viewfinder.anchor = Anchor.topLeft
+    //   ..viewport.position = Vector2(500, 0);
 
-      // addAll([world_RF, cameraComponent_RF]);
+    //   // ..viewport.size = Vector2(100, 50)  
+
+    // addAll([world_RF]);
 
     return super.onLoad();
   }
