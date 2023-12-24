7119d42dfc8c3dd51888493c3ee4ab72354e6d23
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-24 13:57:44 -0300

    Autocommit a cada 15 minutos

--- lib/games/super_mario_bros_game.dart
+++ lib/games/super_mario_bros_game.dart
@@ -1,8 +1,9 @@
 // import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:recre_mario_bros_flutter/constants/globals.dart';
-import 'package:flame/components.dart';
+
 
 class SuperMarioBrosGame extends FlameGame {
   // RF Criando as variaveis todos os componetes destas variaveis abaixo estão no (package:flame/camera.dart)
@@ -16,20 +17,23 @@ class SuperMarioBrosGame extends FlameGame {
       Globals.lv_1_1,
       Vector2.all(Globals.tileSize),
     );
-    add(map_RF);
-    // cameraComponent_RF = CameraComponent(world: world_RF)
-    //   // RF Criar componente de cameras
-    //   /* visor controla qual parte do mundo esta sendo vista através da janela de visualização
-    //   a janela de visualização é essencialmente uma janela a partir da qual o jogo é observado
-    //   */
-    //   ..viewfinder.visibleGameSize = Vector2(50, 50)
-    //   ..viewfinder.position = Vector2(0, 0)
-    //   ..viewfinder.anchor = Anchor.topLeft
-    //   ..viewport.position = Vector2(500, 0);
+    world_RF.add(map_RF);
+
+    cameraComponent_RF = CameraComponent(world: world_RF)
+      // RF Criar componente de cameras
+      /* visor controla qual parte do mundo esta sendo vista através da janela de visualização
+      a janela de visualização é essencialmente uma janela a partir da qual o jogo é observado
+      */
+      // RF Posiciona o mundo no eixo vertical
+      ..viewfinder.visibleGameSize = Vector2(450, 0)
+      // RF Posiciona  o mundo no eixo vertical
+      ..viewfinder.position = Vector2(120, 0)
+      ..viewfinder.anchor = Anchor.topLeft
+      ..viewport.position = Vector2(500, 0);
 
     //   // ..viewport.size = Vector2(100, 50)  
 
-    // addAll([world_RF]);
+    addAll([world_RF, cameraComponent_RF]);
 
     return super.onLoad();
   }
