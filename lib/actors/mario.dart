5997b73e21da2f8c9ae9661f1031841467cda3a5
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-29 16:12:44 -0300

    Autocommit a cada 15 minutos

--- lib/actors/mario.dart
+++ lib/actors/mario.dart
@@ -4,12 +4,40 @@
  */
 import 'package:flame/components.dart';
 import 'package:flame/experimental.dart';
+import 'package:recre_mario_bros_flutter/constants/animation_configs.dart';
+import 'package:recre_mario_bros_flutter/constants/globals.dart';
 
-enum MarioAnimationsState_RF{
+enum MarioAnimationsState_RF {
   idle,
   walking,
   jumping,
 }
-class Mario extends SpriteAnimationGroupComponent<MarioAnimationsState_RF>{
-  Mario({required Vector2 position, required Rectangle})
+
+class Mario extends SpriteAnimationGroupComponent<MarioAnimationsState_RF> {
+  Mario({required Vector2 position, required Rectangle levelBounds})
+      : super(
+          position: position,
+          size: Vector2(Globals.tileSize, Globals.tileSize),
+          anchor: Anchor.topLeft,
+        ) {
+          debugMode=true;
+        }
+        //RF especificar qual animação inicia o mario
+        @override
+        Future<void>? onLoad() async{
+          
+          //RF Acessar as configurações de animação 
+          final SpriteAnimation idle = await AnimationConfigs.mario.idle();
+          //RF mapeando cada uma das animações
+
+          animations = {
+            MarioAnimationsState_RF.idle: idle,
+          };
+
+          current = MarioAnimationsState_RF.idle;
+
+
+          return super.onLoad();
+        }
+
 }
