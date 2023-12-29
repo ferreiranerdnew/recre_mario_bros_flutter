68a4b0121ac845ae9d7e985a31ef463033104977
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-29 15:57:44 -0300

    Autocommit a cada 15 minutos

--- lib/actors/mario.dart
+++ lib/actors/mario.dart
@@ -2,9 +2,14 @@
  * Classe responsavel pro criar todas as animações do super mario 
  * 
  */
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 
 enum MarioAnimationsState_RF{
   idle,
   walking,
   jumping,
+}
+class Mario extends SpriteAnimationGroupComponent<MarioAnimationsState_RF>{
+  Mario({required Vector2 position, required Rectangle})
 }
