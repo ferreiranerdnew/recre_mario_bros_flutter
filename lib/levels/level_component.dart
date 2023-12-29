2c00d9478786499a8ec3f0e464a20a5afcc8eea2
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-29 14:57:50 -0300

    Autocommit a cada 15 minutos

--- lib/levels/level_component.dart
+++ lib/levels/level_component.dart
@@ -5,6 +5,7 @@ import 'package:flame/components.dart';
 import 'package:recre_mario_bros_flutter/constants/globals.dart';
 import 'package:recre_mario_bros_flutter/games/super_mario_bros_game.dart';
 import 'package:recre_mario_bros_flutter/levels/level_option.dart';
+import 'package:recre_mario_bros_flutter/objects/platform.dart';
 
 class LevelComponent extends Component with HasGameRef<SuperMarioBrosGame> {
   final LevelOption option;
@@ -29,7 +30,26 @@ class LevelComponent extends Component with HasGameRef<SuperMarioBrosGame> {
               level_RF.tileMap.map.height.toDouble()) *
           Globals.tileSize,
     );
-
+    // iniciando chamada da função createPlatforms
+    createPlatforms(level_RF.tileMap);
     return super.onLoad();
+  }
+
+  void createPlatforms(RenderableTiledMap tileMap) {
+    ObjectGroup? platformsLayer = tileMap.getLayer<ObjectGroup>('Platforms');
+
+    if (platformsLayer == null) {
+      throw Exception(
+          'inglês: Platforms Layer could not be found; português: A camada de plataformas não foi encontrada');
+    }
+
+    for (final TiledObject obj in platformsLayer.objects) {
+      //esta Platform é uma classe nossa do nosso import package:recre_mario_bros_flutter/objects/platform.dart
+      Platform platform = Platform(
+        position: Vector2(obj.x, obj.y),
+        size: Vector2(obj.width, obj.height),
+      );
+      gameRef.world_RF.add(platform);
+    }
   }
 }
